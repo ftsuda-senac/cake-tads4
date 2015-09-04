f9b0217f4c0d59ded6e14c86c0e72a77c1d0ff79
fernando.tsuda <fernando.tsuda@CAS419569A10736.senacsp.edu.br>
2015-09-03 22:34:48 -0300

    Estrutura inicial do projeto

---
+++ cake-web-jsf2/src/main/java/br/senac/tads/dsw/cake/jsf2/managedbean/CategoriaBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.cake.jsf2.managedbean;
+
+import javax.inject.Named;
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ *
+ * @author fernando.tsuda
+ */
+@Named(value = "categoriaBean")
+@ApplicationScoped
+public class CategoriaBean {
+
+    /**
+     * Creates a new instance of CategoriaBean
+     */
+    public CategoriaBean() {
+    }
+    
+}
---
+++ cake-web-jsf2/src/main/java/br/senac/tads/dsw/cake/jsf2/managedbean/ProdutoBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.cake.jsf2.managedbean;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author fernando.tsuda
+ */
+@Named(value = "produtoBean")
+@RequestScoped
+public class ProdutoBean {
+
+    /**
+     * Creates a new instance of ProdutoBean
+     */
+    public ProdutoBean() {
+    }
+    
+}
