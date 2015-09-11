22d1796c70dc056986e125e8f05130f0b32249da
fernando.tsuda <fernando.tsuda@CAS419569A10736.senacsp.edu.br>
2015-09-10 22:39:41 -0300

    Projeto desenvolvido em aula.

--- cake-web-jsf2/src/main/java/br/senac/tads/dsw/cake/jsf2/managedbean/CategoriaBean.java
+++ cake-web-jsf2/src/main/java/br/senac/tads/dsw/cake/jsf2/managedbean/CategoriaBean.java
@@ -5,6 +5,10 @@
  */
 package br.senac.tads.dsw.cake.jsf2.managedbean;
 
+import br.senac.tads.cake.common.entity.Categoria;
+import br.senac.tads.cake.common.service.CategoriaService;
+import br.senac.tads.cake.common.service.fakeimpl.CategoriaServiceFakeImpl;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.ApplicationScoped;
 
@@ -20,6 +24,11 @@ public class CategoriaBean {
      * Creates a new instance of CategoriaBean
      */
     public CategoriaBean() {
+    }
+    
+    public List<Categoria> getLista() {
+        CategoriaService service = new CategoriaServiceFakeImpl();
+        return service.listar();
     }
     
 }
--- cake-web-jsf2/src/main/java/br/senac/tads/dsw/cake/jsf2/managedbean/ProdutoBean.java
+++ cake-web-jsf2/src/main/java/br/senac/tads/dsw/cake/jsf2/managedbean/ProdutoBean.java
@@ -5,21 +5,37 @@
  */
 package br.senac.tads.dsw.cake.jsf2.managedbean;
 
+import br.senac.tads.cake.common.entity.Produto;
+import br.senac.tads.cake.common.service.ProdutoService;
+import br.senac.tads.cake.common.service.fakeimpl.ProdutoServiceFakeImpl;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  *
  * @author fernando.tsuda
  */
 @Named(value = "produtoBean")
-@RequestScoped
-public class ProdutoBean {
+@SessionScoped
+public class ProdutoBean implements Serializable {
+    
+    private List<Produto> xptomdbmad;
 
     /**
      * Creates a new instance of ProdutoBean
      */
     public ProdutoBean() {
+    }
+
+    /**
+     * @return the lista
+     */
+    public List<Produto> getLista() {
+        ProdutoService service = new ProdutoServiceFakeImpl();
+        xptomdbmad = service.listar(0, 100);
+        return xptomdbmad;
     }
     
 }
