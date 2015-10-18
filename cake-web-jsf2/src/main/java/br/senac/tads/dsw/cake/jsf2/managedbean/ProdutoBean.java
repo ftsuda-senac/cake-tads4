b85d77e4309c305273f554bb897df3427c850608
Fernando <Fernando@TSUDA005.home>
2015-10-18 14:25:26 -0200

    Correção na persistência da imagem (associação produto - imagem)

--- cake-web-jsf2/src/main/java/br/senac/tads/dsw/cake/jsf2/managedbean/ProdutoBean.java
+++ cake-web-jsf2/src/main/java/br/senac/tads/dsw/cake/jsf2/managedbean/ProdutoBean.java
@@ -35,198 +35,201 @@ import javax.servlet.http.Part;
 @ManagedBean
 @RequestScoped
 public class ProdutoBean implements Serializable {
+  
+  @ManagedProperty(value = "#{param.id}") // Permite associar um parametro passado via URL na requisicao
+  private Long idProd;
+  
+  private String nome;
+  
+  private String descricao;
+  
+  private List<Long> idsCategorias;
+  
+  private BigDecimal preco;
+  
+  private List<Produto> listaProdutos;
+  
+  private Part imagem;
+  
+  private Produto pTemp;
 
-    @ManagedProperty(value = "#{param.id}") // Permite associar um parametro passado via URL na requisicao
-    private Long idProd;
+  /**
+   * Creates a new instance of ProdutoBean
+   */
+  public ProdutoBean() {
+  }
 
-    private String nome;
-
-    private String descricao;
-
-    private List<Long> idsCategorias;
-
-    private BigDecimal preco;
-
-    private List<Produto> listaProdutos;
-
-    private Part imagem;
-
-    private Produto pTemp;
-
-    /**
-     * Creates a new instance of ProdutoBean
-     */
-    public ProdutoBean() {
-    }
-
-    /**
-     * @return the lista
-     */
-    public List<Produto> getLista() {
-        ProdutoService service = new ProdutoServiceJPAImpl();
-        setListaProdutos(service.listar(0, 100));
-        return getListaProdutos();
-    }
-
-    public Produto getProduto() {
-        /*
-         String id = FacesContext.getCurrentInstance()
-         .getExternalContext()
-         .getRequestParameterMap().get("id");
+  /**
+   * @return the lista
+   */
+  public List<Produto> getLista() {
+    ProdutoService service = new ProdutoServiceJPAImpl();
+    setListaProdutos(service.listar(0, 100));
+    return getListaProdutos();
+  }
+  
+  public Produto getProduto() {
+    /*
+     String id = FacesContext.getCurrentInstance()
+     .getExternalContext()
+     .getRequestParameterMap().get("id");
                 
-         Long idProd = Long.parseLong(id);*/
-        ProdutoService service = new ProdutoServiceJPAImpl();
-        return service.obter(idProd);
+     Long idProd = Long.parseLong(id);*/
+    ProdutoService service = new ProdutoServiceJPAImpl();
+    return service.obter(idProd);
+  }
+  
+  public List<Produto> getListaProdutos() {
+    return listaProdutos;
+  }
+  
+  public void setListaProdutos(List<Produto> listaProdutos) {
+    this.listaProdutos = listaProdutos;
+  }
+  
+  public Long getIdProd() {
+    return idProd;
+  }
+  
+  public void setIdProd(Long idProd) {
+    this.idProd = idProd;
+  }
+  
+  public String getNome() {
+    return nome;
+  }
+  
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  
+  public String getDescricao() {
+    return descricao;
+  }
+  
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+  
+  public List<Long> getIdsCategorias() {
+    return idsCategorias;
+  }
+  
+  public void setIdsCategorias(List<Long> idsCategorias) {
+    this.idsCategorias = idsCategorias;
+  }
+  
+  public BigDecimal getPreco() {
+    return preco;
+  }
+  
+  public void setPreco(BigDecimal preco) {
+    this.preco = preco;
+  }
+  
+  public Part getImagem() {
+    return imagem;
+  }
+  
+  public void setImagem(Part imagem) {
+    this.imagem = imagem;
+  }
+  
+  public String salvar() {
+    Produto p = new Produto();
+    CategoriaService cServ = new CategoriaServiceJPAImpl();
+    
+    p.setNome(nome);
+    p.setDescricao(descricao);
+    List<Categoria> listaCategorias = new ArrayList<Categoria>();
+    for (Long idCat : idsCategorias) {
+      Categoria cat = cServ.obter(idCat);
+      listaCategorias.add(cat);
+      cat.setProdutos(Arrays.asList(p));
     }
-
-    public List<Produto> getListaProdutos() {
-        return listaProdutos;
+    p.setCategorias(listaCategorias);
+    p.setPreco(preco);
+    p.setDtCadastro(new Date());
+    
+    ImagemProduto imagem = new ImagemProduto();
+    String nomeArquivo = obterNomeArquivo();
+    if (nomeArquivo != null && nomeArquivo.trim().length() > 0) {
+      salvarImagem(nomeArquivo);
+      imagem.setNomeArquivo(nomeArquivo);
+      imagem.setProduto(p);
+      p.setImagens(Arrays.asList(imagem));
     }
-
-    public void setListaProdutos(List<Produto> listaProdutos) {
-        this.listaProdutos = listaProdutos;
-    }
-
-    public Long getIdProd() {
-        return idProd;
-    }
-
-    public void setIdProd(Long idProd) {
-        this.idProd = idProd;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public List<Long> getIdsCategorias() {
-        return idsCategorias;
-    }
-
-    public void setIdsCategorias(List<Long> idsCategorias) {
-        this.idsCategorias = idsCategorias;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
-
-    public Part getImagem() {
-        return imagem;
-    }
-
-    public void setImagem(Part imagem) {
-        this.imagem = imagem;
-    }
-
-    public String salvar() {
-        Produto p = new Produto();
-        CategoriaService cServ = new CategoriaServiceJPAImpl();
-
-        p.setNome(nome);
-        p.setDescricao(descricao);
-        List<Categoria> listaCategorias = new ArrayList<Categoria>();
-        for (Long idCat : idsCategorias) {
-            listaCategorias.add(cServ.obter(idCat));
+    
+    ProdutoService produtoService = new ProdutoServiceJPAImpl();
+    produtoService.incluir(p);
+    
+    pTemp = p;
+    
+    Flash flash = FacesContext.getCurrentInstance()
+            .getExternalContext().getFlash();
+    flash.put("msg", "Produto cadastrado com sucesso");
+    flash.put("prod", pTemp);
+    
+    return "lista.xhtml?faces-redirect=true";
+  }
+  
+  private String obterNomeArquivo() {
+    if (imagem != null) {
+      String partHeader = imagem.getHeader("content-disposition");
+      for (String content : partHeader.split(";")) {
+        if (content.trim().startsWith("filename")) {
+          String nomeArquivo = content.substring(content.indexOf('=') + 1)
+                  .trim().replace("\"", "");
+          int lastFilePartIndex = nomeArquivo.lastIndexOf("\\");
+          if (lastFilePartIndex > 0) {
+            return nomeArquivo.substring(lastFilePartIndex, nomeArquivo.length());
+          }
+          return nomeArquivo;
         }
-        p.setCategorias(listaCategorias);
-        p.setPreco(preco);
-        p.setDtCadastro(new Date());
-
-        ImagemProduto imagem = new ImagemProduto();
-        String nomeArquivo = obterNomeArquivo();
-        if (nomeArquivo != null && nomeArquivo.trim().length() > 0) {
-            salvarImagem(nomeArquivo);
-            imagem.setNomeArquivo(nomeArquivo);
-            p.setImagens(Arrays.asList(imagem));
-        }
-
-        ProdutoService produtoService = new ProdutoServiceJPAImpl();
-        produtoService.incluir(p);
-
-        pTemp = p;
-
-        Flash flash = FacesContext.getCurrentInstance()
-                .getExternalContext().getFlash();
-        flash.put("msg", "Produto cadastrado com sucesso");
-        flash.put("prod", pTemp);
-
-        return "lista.xhtml?faces-redirect=true";
+      }
     }
-
-    private String obterNomeArquivo() {
-        if (imagem != null) {
-            String partHeader = imagem.getHeader("content-disposition");
-            for (String content : partHeader.split(";")) {
-                if (content.trim().startsWith("filename")) {
-                    String nomeArquivo = content.substring(content.indexOf('=') + 1)
-                            .trim().replace("\"", "");
-                    int lastFilePartIndex = nomeArquivo.lastIndexOf("\\");
-                    if (lastFilePartIndex > 0) {
-                        return nomeArquivo.substring(lastFilePartIndex, nomeArquivo.length());
-                    }
-                    return nomeArquivo;
-                }
-            }
-        }
-        return null;
-    }
-
-    private void salvarImagem(String nomeImagem) {
-        String diretorioDestino = "C:" + File.separator + "desenv"
-                + File.separator + "imagens" + File.separator;
-        File arquivo = new File(diretorioDestino + nomeImagem);
-
-        InputStream inputStream = null;
-        OutputStream outputStream = null;
-
+    return null;
+  }
+  
+  private void salvarImagem(String nomeImagem) {
+    String diretorioDestino = "C:" + File.separator + "desenv"
+            + File.separator + "imagens" + File.separator;
+    File arquivo = new File(diretorioDestino + nomeImagem);
+    
+    InputStream inputStream = null;
+    OutputStream outputStream = null;
+    
+    try {
+      inputStream = imagem.getInputStream();
+      outputStream = new FileOutputStream(arquivo);
+      
+      int read = 0;
+      final byte[] bytes = new byte[1024];
+      while ((read = inputStream.read(bytes)) != -1) {
+        outputStream.write(bytes, 0, read);
+      }
+    } catch (IOException e) {
+      //TODO: LOGAR ERRO
+    } finally {
+      if (outputStream != null) {
         try {
-            inputStream = imagem.getInputStream();
-            outputStream = new FileOutputStream(arquivo);
-
-            int read = 0;
-            final byte[] bytes = new byte[1024];
-            while ((read = inputStream.read(bytes)) != -1) {
-                outputStream.write(bytes, 0, read);
-            }
+          outputStream.close();
         } catch (IOException e) {
-            //TODO: LOGAR ERRO
-        } finally {
-            if (outputStream != null) {
-                try {
-                    outputStream.close();
-                } catch (IOException e) {
-                    //TODO: LOGAR ERRO
-                }
-            }
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    //TODO: LOGAR ERRO
-                }
-            }
+          //TODO: LOGAR ERRO
         }
+      }
+      if (inputStream != null) {
+        try {
+          inputStream.close();
+        } catch (IOException e) {
+          //TODO: LOGAR ERRO
+        }
+      }
     }
-
-    public Produto getProdutoTemp() {
-        return pTemp;
-    }
-
+  }
+  
+  public Produto getProdutoTemp() {
+    return pTemp;
+  }
+  
 }
